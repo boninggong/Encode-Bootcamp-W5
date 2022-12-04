a599d4cf88ad14b3dce53a8c14ee91a2ad0dc3ee
Boning Gong <boninggong@yahoo.com>
2022-12-04 13:47:58 +0800

    Loops testing gas and randomness

--- scripts/TestGas.ts
+++ scripts/TestGas.ts
@@ -36,12 +36,33 @@ async function compareLocations() {
   );
 }
 
+async function comparePacking() {
+  const gasContractFactory = await ethers.getContractFactory("Gas");
+  let contract: Gas = await gasContractFactory.deploy();
+  contract = await contract.deployed();
+  const testTx1 = await contract.createUnpacked();
+  const testTx1Receipt = await testTx1.wait();
+  console.log(
+    `Used ${testTx1Receipt.gasUsed} gas units in struct packing test function`
+  );
+  const testTx2 = await contract.createPacked();
+  const testTx2Receipt = await testTx2.wait();
+  console.log(
+    `Used ${testTx2Receipt.gasUsed} gas units in optimized struct packing test function`
+  );
+}
+
 // compareDeploy().catch((err) => {
 //     console.error(err);
 //     process.exitCode = 1;
 // });
 
-compareLocations().catch((err) => {
+// compareLocations().catch((err) => {
+//     console.error(err);
+//     process.exitCode = 1;
+// });
+
+comparePacking().catch((err) => {
     console.error(err);
     process.exitCode = 1;
 });
